fd44e49d912d39a0cf6f7bfa43e8c4309d2542d5
ks8748-code <ks8748@srmist.edu.in>
2026-04-06 08:53:12 +0530

    Week 3 Problems

---
+++ WeeklyAssignmentProblem/src/Week3/Transactionfee.java
@@ -0,0 +1,95 @@
+package Week3;
+import java.util.*;
+
+class Transaction {
+    String id;
+    double fee;
+    String timestamp;
+
+    Transaction(String id, double fee, String timestamp) {
+        this.id = id;
+        this.fee = fee;
+        this.timestamp = timestamp;
+    }
+
+    public String toString() {
+        return id + ":" + fee + "@" + timestamp;
+    }
+}
+
+class Main1 {
+
+    // Bubble Sort (by fee)
+    static void bubbleSort(List<Transaction> list) {
+        int n = list.size();
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).fee > list.get(j + 1).fee) {
+                    Transaction temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+
+    // Insertion Sort (by fee + timestamp)
+    static void insertionSort(List<Transaction> list) {
+        for (int i = 1; i < list.size(); i++) {
+            Transaction key = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && compare(list.get(j), key) > 0) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+
+            list.set(j + 1, key);
+        }
+    }
+
+    // Compare by fee, then timestamp
+    static int compare(Transaction t1, Transaction t2) {
+        if (t1.fee != t2.fee) {
+            return Double.compare(t1.fee, t2.fee);
+        }
+        return t1.timestamp.compareTo(t2.timestamp);
+    }
+
+    // Find high-fee outliers (>50)
+    static List<Transaction> findOutliers(List<Transaction> list) {
+        List<Transaction> outliers = new ArrayList<>();
+
+        for (Transaction t : list) {
+            if (t.fee > 50) {
+                outliers.add(t);
+            }
+        }
+
+        return outliers;
+    }
+
+    public static void main(String[] args) {
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(new Transaction("id1", 10.5, "10:00"));
+        transactions.add(new Transaction("id2", 25.0, "09:30"));
+        transactions.add(new Transaction("id3", 5.0, "10:15"));
+
+        bubbleSort(transactions);
+        System.out.println("Bubble Sort (fee): " + transactions);
+
+        insertionSort(transactions);
+        System.out.println("Insertion Sort (fee+ts): " + transactions);
+
+        List<Transaction> outliers = findOutliers(transactions);
+        System.out.println("High-fee outliers: " + outliers);
+    }
+}
